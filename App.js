9463ccaf47dc7e536e3dc3c2e2fa32dadeb0f716
DeWeese-Michael <76921430+DeWeese-Michael@users.noreply.github.com>
2024-03-07 13:31:11 -0600

    MD AboutUs and ResultsPage wireframe

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; // Import NavigationContainer MD = Michael DeWeese
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Double check that import in correct MD
 import { StyleSheet, Text, View } from 'react-native';
+import AboutUsPage from './AboutUsPage';//imported from AboutUsPage.js MD
+import ResultsPage from './ResultsPage';//imported from ResultsPage.js MD
+
+
+const Stack = createNativeStackNavigator();//imported from react-navigation/native-stack MD
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer> {/* Wrap EthicsBotin a NavigationContainer */}//Wrap the entire app in a NavigationContainer MD
+      <Stack.Navigator initialRouteName="AboutUs">//Defining AboutUs route MD
+        {/* 'About Us''Results' */}//Defining AboutUs and Results screens MD
+        <Stack.Screen name="AboutUs" component={AboutUsPage} options={{ title: 'About Us' }} /> //Defining AboutUs screen MD
+        <Stack.Screen name="Results" component={ResultsPage} options={{ title: 'Results' }} /> //Defining Results screen MD
+      </Stack.Navigator>//Defining the stack navigator MD
+      <StatusBar style="auto" /> {/* StatusBar can stay outside NavigationContainer*/}
+    </NavigationContainer> //Close NavigationContainer MD
+
+    /* Commented the below out because I am not familiar enough to  delete and needed to add Navigation to set up wireframe. MD */
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
